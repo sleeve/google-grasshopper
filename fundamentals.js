488476af6bc4bd23f1ab2ca1cebebcfb82ab7871
Steve Morris <steve@ajn.in>
2019-10-22 09:22:40 -0700

    arrays and if statements

--- fundamentals.js
+++ fundamentals.js
@@ -97,8 +97,141 @@ print(x);
 // 5
 
 // Arrays
+//
 
 // Magic Answers
+var answer = pickRandom([
+    'yes',
+    'no',
+    'maybe'
+]);
+print(answer);
+
+// Bake a Cake
+var ingredients = [
+    'flour', 
+    'sugar', 
+    'eggs'
+];
+cook(ingredients);
+
+// Whose Shoes
+// Which variable is storing an array
+var myShoes = 'sneakers, highHeels, pumps';
+var yourShoes = ['sneakers', 'highHeels', 'pumps']
+var shoeColor = drawBox(blue)
+// yourShoes is storing the array
+
+// Checkerboard
+var colors = [
+    'black',
+    'white'
+];
+drawBox(colors[0]);
+drawBox(colors[1]);
+drawBox(colors[0]);
+newLine();
+drawBox(colors[1]);
+drawBox(colors[0]);
+drawBox(colors[1]);
+newLine();
+drawBox(colors[0]);
+drawBox(colors[1]);
+drawBox(colors[0]);
+
+// If Statements
+//
+
+// If Editor
+var color = pickRandom([
+    blue,
+    green
+]);
+print(color);
+if (color === blue) {
+    drawBox(blue);
+}
+
+// Flip a Coin
+var answer = pickRandom([
+    'heads',
+    'tails'
+]);
+print(answer);
+if (answer === 'heads') {
+    drawBoxes('gwg ggg gwg');
+}
+if (answer === 'tails') {
+    drawBoxes('rrr wrw wrw');
+}
+
+// Is it Happy?
+// What code would check if the grasshopper variable is holding the string 'happy'?
+var grasshopper = 'happy';
+// grasshopper === 'happy';
+
+// Flip a Coin Again
+var answer = pickRandom([
+    'heads',
+    'tails'
+]);
+print(answer);
+if (answer === 'heads') {
+    drawBoxes('gwg ggg gwg');
+} else {
+    drawBoxes('rrr wrw wrw');
+}
+
+// Guessing Game
+var pea = pickRandom([
+    1,
+    2,
+    3
+]);
+if (pea !== 1) {
+    print('Not under shell 1')
+}
+if (pea !== 3) {
+    print('Not under shell 3')
+}
+
+// What's the Weather?
+// What will this code print out?
+var todayWeather = 'rainy';
+var tomorrowWeather = 'cloudy';
+if (todayWeather === 'rainy') {
+    print('Bring an umbrella');
+}
+if (todayWeather !== 'rainy') {
+    print('Maybe the sun will come out');
+}
+// It will print 'Bring an umbrella'
+
+// Which Bananas?
+// Which pair of value would let the code print 'Let's make banana bread'?
+var bananas;
+var numberBananas;
+if (bananas !== 'green') {
+    if (numberBananas > 5) {
+        print("Let's make banana bread");
+    }
+}
+// bananas = 'yellow';
+// numberOfBananas = 6;
+
+// Operators
+//
+
+// Modifier Math
+
+
+
+
+
+
+
+
+
 
 
 
