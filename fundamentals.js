e2f1ed3f6d027df01d3835fe4208a8369fb152ca
Steve Morris <steve@ajn.in>
2019-10-22 08:21:15 -0700

    fundamentals work

---
+++ fundamentals.js
@@ -0,0 +1,121 @@
+// Fundamentals
+
+// Drawing shapes
+// French Flag
+drawBox(blue);
+drawBox(white);
+drawBox(red);
+newLine();
+drawBox(blue);
+drawBox(white);
+drawBox(red);
+
+// Gabonese Flag
+drawBox(green);
+drawBox(green);
+drawBox(green);
+newLine();
+drawBox(yellow);
+drawBox(yellow);
+drawBox(yellow);
+newLine();
+drawBox(blue);
+drawBox(blue);
+drawBox(blue);
+
+// How Many Blue?
+// How many blue boxes will be drawn from this code?
+drawBox(orange);
+drawBox(blue);
+orange;
+newLine();
+drawBox(orange);
+blue;
+// 1 blue box will be drawn
+
+// Gabonese Flag Helper
+drawBoxes('ggg');
+newLine();
+drawBoxes('yyy');
+newLine();
+drawBoxes('bbb');
+
+// Gabonese Flag Shortcuts
+drawBoxes('ggg yyy bbb');
+
+// B Is for Blue?
+// How many blue boxes will be created with this code?
+drawBoxes('bob r r r');
+// 2 blue boxes will be created
+
+// A Dash of Random
+drawBox(pickRandom(color));
+drawBox(pickRandom(color));
+
+// Variables
+// Poland to San Marino
+var x = white;
+drawBox(x);
+drawBox(x);
+drawBox(x);
+newLine();
+x = blue;
+drawBox(x);
+drawBox(x);
+drawBox(x);
+
+// Today Is the Day
+var month = 'August';
+print(month);
+var day = 'Friday';
+print(day);
+
+// Which Drinks
+// What will be the output of this code?
+var drink = 'juice';
+print(drink);
+drink = 'water';
+print(drink);
+// Output will be
+// juice
+// water
+
+// Which Word
+// What will be the output of the following code?
+var word = 'alphabet';
+var alphabet = 'word';
+print(alphabet);
+// Output will be
+// word
+
+// What's x?
+// What will be the output of the following code?
+var y = 5;
+var x = y;
+print(x);
+// Output will be
+// 5
+
+// Arrays
+
+// Magic Answers
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
