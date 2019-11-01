6154aed0271dfef0e50c632e4ad0f54495963bc8
Steve Morris <steve@ajn.in>
2019-10-31 20:51:08 -0700

    add money changer

--- fundamentals2.js
+++ fundamentals2.js
@@ -115,8 +115,16 @@ console.log('and also works outside Grasshopper.')
 //
 
 // Money Changer
+function inYen(dollars) {
+    let amount = dollars * 113;
+    return amount;
+};
+console.log('20 US dollars is:');
+console.log(inYen(20) + ' Japanese yen');
+console.log('100 US dollars is:');
+console.log(inYen(100) + ' Japanese yen');
 
-
+// A Little More Change
 
 
 
